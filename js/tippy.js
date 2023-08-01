6fe414efa99c308830e374460369920db7578b79
Mohammed Hamdy <126825747+MohammedHamdyAllam@users.noreply.github.com>
2023-08-01 21:32:26 +0300

    Add files via upload

---
+++ js/tippy.js
@@ -0,0 +1,4 @@
+// tippy("#title", {
+//     content: "this programming languages is important an no one can build website witout it",
+//     allowHTML: true,
+// });
---
+++ js/wifi.js
@@ -0,0 +1,32 @@
+// include elements from html
+let wifi = document.getElementById('wifi');
+let reloadButton = document.getElementById('reload-button');
+
+// window loading sripts
+// online & offline script
+if (window.navigator.onLine) {
+    // nonething
+} else {
+    document.body.innerHTML = '';
+    document.body.appendChild(wifi);
+    wifi.style.display = 'flex';
+}
+
+// online & offline script
+window.addEventListener('online', function () {
+    window.location.reload();
+});
+window.addEventListener('offline', function () {
+    document.body.innerHTML = '';
+    document.body.appendChild(wifi);
+    wifi.style.display = 'flex';
+});
+
+// reload button script
+reloadButton.onclick = function () {
+    reloadButton.style.display = 'none';
+    loader.style.display = 'block';
+    setTimeout(function(){
+        window.location.reload();
+    }, 1000)
+};
